af0c790dd52f95e58632ef03e1fe6fa206a89fc8
Aleksov2020 <aleksov2020@gmail.com>
2023-02-26 09:43:19 +0400

    refactor

--- open_insta_account.bat
+++
@@ -1,8 +0,0 @@
-cd C:\Program Files\Microvirt\MEmu
-memuc -i 1 adb shell input tap 357 1151
-TIMEOUT /T 1 /NOBREAK
-memuc -i 1 adb shell input tap 499 587
-TIMEOUT /T 1 /NOBREAK
-memuc -i 1 adb shell input tap 215 1251
-TIMEOUT /T 2 /NOBREAK
-memuc -i 1 adb shell input tap 269 81
